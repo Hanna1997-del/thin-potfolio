577f1c1f809295a68b459ab3ef119e8c5f87e144
Hanna1997-del <hninthethmue8@gmail.com>
2024-04-26 15:00:34 +0630

    fix nav

--- components/Card.tsx
+++ components/Card.tsx
@@ -63,7 +63,7 @@ const Card: React.FC<CardProps> = () => {
               </div>
             </motion.div>
 
-            <div  className="absolute top-[1.7rem] right-[1rem] z-20 cursor-pointer lg:hidden w-full  rounded-full flex  justify-center items-center shadow "
+            <div  className="absolute top-[1.7rem] left-[8rem] z-20 cursor-pointer lg:hidden w-full  rounded-full flex  justify-center items-center shadow "
             onClick={() => setIsOpen((pre) => !pre)} >
               <GiHamburgerMenu size={30} className={clsx(open? "text-black " : "text-white ")}  />
               
